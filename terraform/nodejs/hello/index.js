51557dcaa03b84077f10548d75dfc4fba9bacc86
Daniel Sidauruk <macbook@Daniel.local>
2023-12-16 18:08:27 +0700

    chore: edit step for cloudbuild.yaml

--- terraform/nodejs/hello/index.js
+++ terraform/nodejs/hello/index.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.get('/api/hello', (req, res) => {
   const name = process.env.NAME || 'World'
-  res.send(`Hello ${name}!`);
-      // res.send(JSON.stringify(req.headers));
+  res.send(`Hello ${name}! This is from NodeJs App`);
 });
 
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log(`hello: listening on port ${port}`);
 });
